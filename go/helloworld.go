b17661323165424f5630005f73a7e2fea3380cc9
matthewsbenjamin <10488814+matthewsbenjamin@users.noreply.github.com>
2018-08-23 21:28:45 +0100

    second loop with second pause

--- go/helloworld.go
+++ go/helloworld.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	for i := 0; i < 200; i++ {
-		fmt.Printf("%d\t%q\t%b\t%#X\n", i, i, i, i)
+		fmt.Printf("%d \t %q \t %b \t %#X \n", i, i, i, i)
 		time.Sleep(time.Second)
 	}
 }
